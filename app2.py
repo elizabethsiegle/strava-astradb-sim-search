4b733a7614e8eee9f6f5dd690003764c4a4078d9
elizabethsiegle <lsiegle@twilio.com>
2024-02-07 12:07:20 -0800

    final?!

--- app2.py
+++ app2.py
@@ -1,20 +1,20 @@
-import streamlit as st
-import base64
 from astrapy.db import AstraDB
+import base64
+from datasets import load_dataset
+from dotenv import load_dotenv
+import instructor
 from langchain.chains.question_answering import load_qa_chain
 from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings
-# from langchain_community.vectorstores import FAISS # faiss-cpu
 from langchain_community.vectorstores import AstraDB as astra
 from langchain_openai import ChatOpenAI
 from langchain_community.callbacks import get_openai_callback
-from datasets import load_dataset
 from openai import OpenAI
-import instructor
-import requests
-from pydantic.main import BaseModel
-from dotenv import load_dotenv
 import os
+from pydantic.main import BaseModel
+import requests
+import streamlit as st
+
 
 class RunDetail(BaseModel):
   WorkoutName: str
@@ -44,7 +44,6 @@ def encode_image(image_file):
 
 # Load API secrets
 load_dotenv()
-LLAMA_PARSE_API_KEY = os.environ.get("LLAMA_PARSE_API_KEY")
 ASTRA_DB_APPLICATION_TOKEN = os.environ.get("ASTRA_DB_APPLICATION_TOKEN")
 ASTRA_DB_API_ENDPOINT = os.environ.get("ASTRA_DB_API_ENDPOINT")
 ASTRA_COLLECTION_NAME = "strava_activities"
@@ -60,20 +59,24 @@ print(f"Connected to Astra DB: {db.get_collections()}")
 
 # Set up Streamlit app
 def main():
-
+    st.image("logosimg.png", width=400)
+    # st.write("🏃‍♀️🚴‍♀️")
+    st.header("Analyze a workout and search other workouts!")
+    st.write("This Python🐍 web🕸️ app is built👩🏻‍💻 w/ [Streamlit](https://streamlit.io/), [OpenAI's new Vision👀 API](https://platform.openai.com/docs/guides/vision), [LangChain](https://www.langchain.com/)⛓️, [Strava API](https://developers.strava.com/), and [Astra DB](https://www.datastax.com/products/datastax-astra)")
     # User input: PDF file upload
     strava_img= st.file_uploader("Upload a screenshot of a Strava workout!🔥⤵️", type=["png", "jpg","jpeg"])
-    if strava_img is not None and st.button('enter'):
+    ret_workouts_specific_input = st.text_area("workout parameters to return from your Strava", placeholder= "runs with moving_time over 4000 and total_elevation_gain over 150")
+    if strava_img is not None and ret_workouts_specific_input is not None and st.button('enter✅'):
         st.image(strava_img)
         
-        with st.spinner('Processing📈...'):
+        with st.spinner('Processing image📈...'):
             base64_image = encode_image(strava_img)
 
             headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}"
             }
-
+            # gpt4-vision
             payload = {
             "model": "gpt-4-vision-preview",
             "messages": [
@@ -102,6 +105,7 @@ def main():
 
             response_json = response.json()
             content = response_json['choices'][0]['message']['content']
+            print(f'content {content}')
 
             detect_workout = client.chat.completions.create(
                 model="gpt-4",
@@ -115,16 +119,16 @@ def main():
             workout_prompt = ''
             if "run" in str(detect_workout):
                 resp_model = RunDetail
-                workout_prompt = "Extract distance, pace, time, and achievements from the following json"
+                workout_prompt = "Extract distance, pace, time, and achievements from the following json: " + content
             elif "swim" in str(detect_workout):
                 resp_model = SwimDetail
-                workout_prompt = "Extract distance, time, and pace from the following json" + content
+                workout_prompt = "Extract distance, time, and pace from the following json: " + content
             elif "ride" in str(detect_workout):
                 resp_model = RideDetail
-                workout_prompt = "Extract distance, time, and elevation gain from the following json" + content
+                workout_prompt = "Extract distance, time, and elevation gain from the following json: " + content
             else:
                 resp_model = RunDetail
-                workout_prompt = "Extract distance, pace, time, and achievements from the following json"+ content
+                workout_prompt = "Extract distance, pace, time, and achievements from the following json: "+ content
 
             workout_info = client.chat.completions.create(
                 model="gpt-4",
@@ -137,45 +141,46 @@ def main():
             )
             st.write(workout_info) #extract from image
 
-            embedding_function = OpenAIEmbeddings()
             strava_huggingface_dataset = load_dataset("lizziepika/strava_activities_runs")["train"]
-            print(f"An example entry: {strava_huggingface_dataset[0]}")
+            print(f"An example entry from Hugging Face dataset: {strava_huggingface_dataset[0]}")
 
             docs = []
             for entry in strava_huggingface_dataset:
                 metadata = {"moving_time": entry["moving_time"], "total_elevation_gain": entry["total_elevation_gain"]}
             
-                # Add a LangChain document with the quote and metadata tags
+                # Add a LangChain document with the name and metadata tags
                 doc = Document(page_content=entry["name"], metadata=metadata)
                 docs.append(doc)
-            print(f'docs {docs}')
-        
+            print(f'docs {docs}') 
+            
+            embedding_function = OpenAIEmbeddings()
             vstore = astra(
                 embedding=embedding_function,
                 collection_name="test",
                 api_endpoint=ASTRA_DB_API_ENDPOINT,
                 token=ASTRA_DB_APPLICATION_TOKEN,
             )
-            inserted_ids = vstore.add_documents(docs)
+            inserted_ids = vstore.add_documents(docs) # add Strava data to vector store
             print(f"\nInserted {len(inserted_ids)} documents.")
 
-            query = "return a workout with moving_time over 4000 and total_elevation_gain over 150"
-            result = vstore.similarity_search(query)
+            result = vstore.similarity_search(ret_workouts_specific_input, k = 3) # "return workouts with moving_time over 4000 and total_elevation_gain over 150"
             print(f'result {result}')
             
             gen_dm = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system", "content": "You are a helpful athletic trainer"},
-                    {"role": "user", "content": f"Recommend a workout related to + {str(result)} and {workout_info}"}
+                    {"role": "user", "content": f"Recommend a workout for someone who has wants workouts similar to the following workouts + {str(result)} and this one as well {workout_info}. Be specific about goals, times, and distances"}
                 ]
             )
             werkout = gen_dm.choices[0].message.content
             
             html_str = f"""
-            <p style="font-family:Arial; color:Pink; font-size: 16px;">Workout like: {werkout}</p>
+            <p style="font-family:Arial; color:Pink; font-size: 16px;">Workout recommendation: {werkout}</p>
             """
         st.markdown(html_str, unsafe_allow_html=True)
+        st.write("Made w/ ❤️ in SF 🌁")
+        st.write("✅ out the [code on GitHub](https://github.com/elizabethsiegle/strava-astradb-sim-search/blob/main/app2.py)")
 
 
 if __name__ == "__main__":
